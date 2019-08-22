fd966d5d1df1a56bd3ec52b1ce0788c7c9e6128a
Busayo Oyewole <busayomioyewole@gmail.com>
2019-08-22 21:45:49 +0200

    jquery workaround

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,8 @@
   -------------------------------------------------------------------------------*/
 
   $(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
+    $('.preloader').fadeOut(1000); // set duration in brackets
   });
-
+  $('.target').click(function() {
+        window.location.href='index.html';
+  });
